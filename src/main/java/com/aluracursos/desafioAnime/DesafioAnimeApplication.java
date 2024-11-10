46aa2d01195d66ff9f2410342c5e029a2325831e
Harold Cortes <harcor59p@gmail.com>
2024-11-10 06:17:34 -0500

    first commit

---
+++ src/main/java/com/aluracursos/desafioAnime/DesafioAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafioAnime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioAnimeApplication.class, args);
+	}
+
+}
